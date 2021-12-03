300f04d7683496f9175ceddbc02a20f31fc44cfd
Grubben <antoniomsprc@gmail.com>
2021-12-03 16:49:29 +0000

    overhaul: after extensive work and studying that genius move -> prety sure I'm done

--- get_next_line.h
+++ get_next_line.h
@@ -12,7 +12,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
+
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
--- overhaul.c
+++ overhaul.c
@@ -9,7 +9,7 @@ char	*get_next_line(int fd)
 	char		*other;
 	ssize_t		reret;
 
-
+	// Input error checker
 	if (fd < 0 || fd > 1024 || BUFFER_SIZE < 1)
 		return (NULL);
 
@@ -26,24 +26,35 @@ char	*get_next_line(int fd)
 
 	if (line == NULL)
 		line = malloc(BUFFER_SIZE + 1);	// if there's stuff inside it'll get deleted
-	reret = read(fd, tmp, BUFFER_SIZE);	
-	if (reret == -1)
-			return (NULL);
-	nlpos = ft_strchr(tmp, '\n');
+
+	nlpos = NULL;
 	while (nlpos == NULL)
 	{
+		reret = read(fd, tmp, BUFFER_SIZE);	
+		if (reret == -1)
+			return (NULL);
+		nlpos = ft_strchr(tmp, '\n');
+		if (reret == 0)
+		{
+			if (nlpos == NULL)
+			{
+				free(line);
+				return (ft_strjoin(line, tmp));
+			}
+			else
+				break ;
+		}
+		tmp[reret] = '\0';
 		new = ft_strjoin(line, tmp);	// not sure what happens if line is empty
 		free(line);
 		line = new;
-		read(fd, tmp, BUFFER_SIZE);	
-		nlpos = ft_strchr(tmp, '\n');
 	}
 	// return (line) up till '\n'
 	// what if nlpos is the last char of line!!!
 	other = ft_substr(tmp, 0, nlpos - tmp + 1 );
 	new = ft_strjoin(line, other);	// string up to n' including '\n'
 	free(line);
-	line = ft_substr(tmp, nlpos - tmp + 1, BUFFER_SIZE); // len = BF is overkill but sufficient for now
+	line = ft_substr(tmp, nlpos - tmp + 1, BUFFER_SIZE); // len = BF is overkill but sufficient for now. It's a max anyway
 
 	return (new);
 }
