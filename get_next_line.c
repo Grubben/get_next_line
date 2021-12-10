51fcc60e73e6d5554fb4d3c5463eedde77e4937c
Manjarc <manjarc@manjarc-macbookpro92.com>
2021-12-10 16:23:29 +0000

    get_next_line: implemented free_ret

--- get_next_line.c
+++ get_next_line.c
@@ -23,19 +23,12 @@ char	*get_next_line(int fd)
 
 	if (line[fd] == NULL)
 	{
-		//line[fd] = calloc(BUFFER_SIZE + 1, sizeof(char));
 		line[fd] = malloc(BUFFER_SIZE + 1);
 		line[fd][BUFFER_SIZE] = 0;
 
 		reret = read(fd, line[fd], BUFFER_SIZE);
-		if (reret <= -1)
-			return (NULL);
-		else if (reret == 0)
-		{
-			free(line[fd]);
-			line[fd] = NULL;
-			return (NULL);
-		}
+		if (reret <= 0)
+			return (free_ret(&line[fd], NULL));
 	}
 
 	nlpos = ft_strchr(line[fd], '\n');	// NOTE it's done on line[fd] not tmp
@@ -46,15 +39,12 @@ char	*get_next_line(int fd)
 		if ((size_t)(nlpos - line[fd]) == ft_strlen(line[fd]) - 1)	// if it's the last character
 		{
 			new = ft_substr(line[fd], 0, ft_strlen(line[fd]));	// This used to be a ft_strdup
-			free(line[fd]);
-			line[fd] = NULL;
-			return (new);
+			return (free_ret(&line[fd], new));
 		}
 		tmp = line[fd];
 		new = ft_substr(line[fd], 0, nlpos - line[fd] + 1 );
 		line[fd] = ft_substr(tmp, nlpos - tmp + 1, BUFFER_SIZE); // len = BF is overkill but sufficient for now
-		free(tmp);
-		return (new);
+		return (free_ret(&tmp, new));
 	}
 	// No '\n' in line[fd]
 	tmp = malloc(BUFFER_SIZE + 1);
@@ -64,16 +54,13 @@ char	*get_next_line(int fd)
 	{
 		reret = read(fd, tmp, BUFFER_SIZE);	
 		if (reret <= -1)
-			return (NULL);
-		// read makes sure this happens by itself by adding weird stuff to last read!!!!!!!!!!
+			return (free_ret(&tmp, NULL));
 		if (reret == 0)
 		{
 			free(tmp);
 			// new = ft_strdup(line[fd]);	//	This can also be expressed as a ft_substr
 			new = ft_substr(line[fd], 0, ft_strlen(line[fd]));
-			free(line[fd]);
-			line[fd] = NULL;
-			return (new);
+			return (free_ret(&line[fd], new));
 		}
 		tmp[reret] = '\0';	// The Golden Trick
 		nlpos = ft_strchr(tmp, '\n');
@@ -89,9 +76,7 @@ char	*get_next_line(int fd)
 	{
 		new = ft_strjoin(line[fd], tmp);
 		free(tmp);
-		free(line[fd]);
-		line[fd] = NULL;
-		return (new);
+		return (free_ret(&line[fd], new));
 	}
 	other = ft_substr(tmp, 0, nlpos - tmp + 1 );	// string up to n' including '\n'	
 	new = ft_strjoin(line[fd], other);	// line[fd] + string up to n' including '\n'
