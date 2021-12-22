8f428ea8bbf7f4351257243bad955e23d83ea529
Grubben <antoniomsprc@gmail.com>
2021-12-22 17:02:59 +0000

    get_next_line: made new function to decrease size. Seems to be working, gotta check on linux

--- get_next_line.c
+++ get_next_line.c
@@ -18,52 +18,57 @@ void	free_chg(char **tofree, char *newval)
 	*tofree = newval;
 }
 
-char	*no_n(int fd, char **line)
+char	*make_new(int fd, char **line, char **adtmp)
 {
-	ssize_t	reret;
 	char	*nlpos;
 	char	*new;
 	char	*tmp;
 
-
-	tmp = malloc(BUFFER_SIZE + 1);
-	tmp[BUFFER_SIZE] = 0;
-
-
-	reret = read(fd, tmp, BUFFER_SIZE);	
-	if (reret <= -1)
-		return (free_ret(&tmp, NULL));
-	if (reret == 0)
-	{
-		free(tmp);
-		tmp = ft_substr(line[fd], 0, ft_strlen(line[fd]));	// == ft_strdup(line[fd])
-		//if (line[fd] != NULL)
-		free_chg(&line[fd], NULL);
-		return (tmp);
-	}
+	tmp = *adtmp;
 	
-	tmp[reret] = '\0';	// The Golden Trick
 
 	nlpos = ft_strchr(tmp, '\n');
 	if (nlpos != NULL)
 	{
 		new = ft_substr(tmp, 0, nlpos - tmp + 1 );	// tmp up to n' including '\n'
 		free_chg(&new, ft_strjoin(line[fd], new));	// all the string up to n' including '\n'
-
-		//	If ft_substr makes it point  NULL if the size isn't good (start == '\0') then just free_chg is necessary
 		if ((size_t)(nlpos - tmp) == ft_strlen(tmp) -1)	// if '\n' is the last character
-			free_ret(&line[fd], NULL);
+			free_chg(&line[fd], NULL);
 		else
 			free_chg(&line[fd], ft_substr(tmp, nlpos - tmp + 1, BUFFER_SIZE)); // len = BF is overkill but sufficient for now. It's a max anyway
-
 		free(tmp);
 		return (new);
 	}
-
 	free_chg(&line[fd], ft_strjoin(line[fd], tmp));
 	free(tmp);
-	
-	return (no_n(fd, line));
+
+	return (NULL);
+}
+
+char	*no_n(int fd, char **line)
+{
+	ssize_t	reret;
+	char	*new;
+	char	*tmp;
+
+
+	tmp = malloc(BUFFER_SIZE + 1);
+	//tmp[BUFFER_SIZE] = 0;
+	reret = read(fd, tmp, BUFFER_SIZE);	
+	if (reret <= -1)
+		return (free_ret(&tmp, NULL));
+	if (reret == 0)
+	{
+		free_chg(&tmp, ft_substr(line[fd], 0, ft_strlen(line[fd])));	// => tmp = ft_strdup(line[fd])
+		free_chg(&line[fd], NULL);
+		return (tmp);
+	}
+	tmp[reret] = '\0';	// The Golden Trick
+
+	new = make_new(fd, line, &tmp);
+	if (new == NULL)
+		return (no_n(fd, line));
+	return (new);
 }
 
 char	*get_next_line(int fd)
