7d827b778d864e1bd9c8228cae6c3c88a0be1c82
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r8s1.42lisboa.com>
2021-12-28 16:32:28 +0000

    utils: bettered strjoin a lot! Removed ft_calloc and ft_memmove

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,28 +6,11 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 18:34:26 by amaria-d          #+#    #+#             */
-/*   Updated: 2021/12/28 16:00:08 by amaria-d         ###   ########.fr       */
+/*   Updated: 2021/12/28 16:33:32 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	char	*ptr;
-	size_t	i;
-
-	ptr = (char *)malloc(count * size);
-	if (!ptr)
-		return (NULL);
-	i = 0;
-	while (i < count * size)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return ((void *)ptr);
-}
 
 size_t	ft_strlen(const char *s)
 {
@@ -95,42 +78,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (ptr);
 }
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	size_t	i;
-
-	if (src < dst)
-	{
-		i = len;
-		while (i > 0)
-		{
-			((char *)dst)[i - 1] = ((char *)src)[i - 1];
-			i--;
-		}
-		return ((void *)dst);
-	}
-	if (dst == NULL && src == NULL)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dst);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
+	size_t	i;
+	size_t	j;
 
 	if (!s1)
 		return (NULL);
 	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!ptr)
 		return (NULL);
-	ft_memmove(ptr, s1, ft_strlen(s1));
-	ft_memmove(ptr + ft_strlen(s1), s2, ft_strlen(s2));
+	j = 0;
+	i = 0;
+	while (s1[i])
+		ptr[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		ptr[j++] = s2[i++];
 	ptr[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	return (ptr);
 }
