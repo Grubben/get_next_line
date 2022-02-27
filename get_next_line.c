638b4d259fa511c129f348a9a326694ceab1aa80
Antonio Maria da silva pinto ribeiro da cunha <amaria-d@c1r8s1.42lisboa.com>
2022-02-27 16:56:54 +0000

    Finished final_prep

--- get_next_line.c
+++ get_next_line.c
@@ -6,96 +6,11 @@
 /*   By: amaria-d <amaria-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 13:31:08 by amaria-d          #+#    #+#             */
-/*   Updated: 2022/02/08 13:31:09 by amaria-d         ###   ########.fr       */
+/*   Updated: 2022/02/27 16:55:44 by amaria-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*free_ret(char **toleave, char *toret)
-{
-	free(*toleave);
-	*toleave = NULL;
-	return (toret);
-}
-
-/* x = b;
- * free(a);
- * ->
- *	free_chg(x, b);
-*/
-void	free_chg(char **tofree, char *newval)
-{
-	free(*tofree);
-	*tofree = newval;
-}
-
-char	*make_new(int fd, char **line, char **adtmp)
-{
-	char	*nlpos;
-	char	*new;
-	char	*tmp;
-
-	tmp = *adtmp;
-	nlpos = ft_strchr(tmp, '\n');
-	if (nlpos != NULL)
-	{
-		new = ft_substr(tmp, 0, nlpos - tmp + 1);
-		free_chg(&new, ft_strjoin(line[fd], new));
-		if ((size_t)(nlpos - tmp) == ft_strlen(tmp) - 1)
-			free_chg(&line[fd], NULL);
-		else
-			free_chg(&line[fd],
-				ft_substr(tmp, nlpos - tmp + 1, ft_strlen(line[fd])));
-		free(tmp);
-		return (new);
-	}
-	free_chg(&line[fd], ft_strjoin(line[fd], tmp));
-	free(tmp);
-	return (NULL);
-}
-
-char	*no_n(int fd, char **line)
-{
-	ssize_t	reret;
-	char	*new;
-	char	*tmp;
-
-	//tmp = ft_substr(line[fd], 0, ft_strlen(line[fd])); //copy
-
-
-		if (reret <= -1)
-		{
-			free(tmp);
-			return (NULL);
-		}
-		if (reret == 0)
-		{
-			free(&tmp);
-			tmp = ft_substr(line[fd], 0, ft_strlen(line[fd]));
-			free(line[fd]);
-			line[fd] = NULL;
-			return (tmp);
-		}
-		tmp[reret] = '\0';
-		new = make_new(fd, line, &tmp);
-	}
-	return (new);
-}
-
-void	emptyRead(int fd, char **line)
-{
-	ssize_t	reret;
-
-	line[fd] = malloc(BUFFER_SIZE + 1);
-	reret = read(fd, line[fd], BUFFER_SIZE);
-	if (reret <= 0)
-	{
-		free(line[fd]);
-		return ;
-	}
-	line[fd][reret] = '\0';
-}
 
 int	get_more(int fd, char **line)
 {
@@ -111,7 +26,7 @@ int	get_more(int fd, char **line)
 	}
 	if (reret = 0)
 	{
-		final_prep(...);
+		final_prep(fd, line, NULL);
 		return (1);
 	}
 	return (2);
@@ -119,6 +34,23 @@ int	get_more(int fd, char **line)
 
 char	*final_prep(int fd, char **line, char *pos)
 {
+	char	*new;
+	char	*rest;
+
+	if (pos == NULL)
+	{
+		new = ft_substr(line[fd], 0, ft_strlen(line[fd])); //copy
+		free(line[fd]);
+		line[fd] = NULL;
+	}
+	else
+	{
+		new = ft_substr(line[fd], 0, pos - line[fd]);
+		rest = ft_substr(line[fd], pos - line[fd] + 1, ft_strlen(line[fd] + pos));
+		free(line[fd]);
+		line[fd] = rest;
+	}
+	return (new);
 
 }
 
